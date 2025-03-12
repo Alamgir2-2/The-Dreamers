d667970ae63961161981c8d39914d8b3252ab62f
Alamgir2-2 <darkthehunter22@gmail.com>
2025-03-12 23:51:37 +0500

    Footer Updated

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -8,12 +8,12 @@ const Footer = () => {
             <div className="container mx-auto px-6 md:px-12">
                 {/* Footer Grid */}
                 <div className=" border-t border-gray-500 pt-6 grid grid-cols-1 md:grid-cols-3 gap-8 text-center md:text-left">
-                    
+
                     {/* Brand Section */}
                     <div>
                         <h2 className="text-2xl font-bold text-green-500"><Link to="/">দ্যা ড্রিমার্স</Link></h2>
                         <p className="mt-2 text-red-500">
-                            একটি শিক্ষার্থী ও সামাজিক কল্যাণমূলক সংগঠন 
+                            একটি শিক্ষার্থী ও সামাজিক কল্যাণমূলক সংগঠন
                         </p>
                     </div>
 
@@ -26,19 +26,26 @@ const Footer = () => {
                                     Home
                                 </Link>
                             </li>
+
+                            <li>
+                                <Link to="/blood" className="hover:text-green-400 transition">
+                                    Blood Bank
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link to="/events" className="hover:text-green-400 transition">
+                                    Events
+                                </Link>
+                            </li>
                             <li>
                                 <Link to="/about" className="hover:text-green-400 transition">
                                     About Us
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/blogs" className="hover:text-green-400 transition">
-                                    Blogs
-                                </Link>
-                            </li>
-                            <li>
                                 <Link to="/contact" className="hover:text-green-400 transition">
-                                    Contact
+                                    Contact Us
                                 </Link>
                             </li>
                         </ul>
