f5741dc384ea59e26a9c1d5283fa88135319d780
Alamgir2-2 <darkthehunter22@gmail.com>
2026-03-02 00:30:46 +0600

    Fix Vercel serverless function

--- api/index.js
+++ api/index.js
@@ -192,4 +192,18 @@ app.get('/api/members', async (req, res) => {
   }
 });
 
-export default app;
+// Export for Vercel serverless
+export default async (req, res) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  return app(req, res);
+};
